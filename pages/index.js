997a96727a1076cb1101c91efaaeeff828450832
hesamwebdesign <hesamwebdesigner@gmail.com>
2023-09-23 22:08:09 +0330

    Set Fade-In for Home Page

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import ProjectBtn from "../components/ProjectsBtn";
 import Avatar from "../components/Avatar";
 
 // Variants
-import { FadeIn } from "../variants";
+import { FadeIn, fadeIn } from "../variants";
 
 const Home = () => {
   return (
@@ -27,20 +27,41 @@ const Home = () => {
           }
         >
           {/*title*/}
-          <h1 className="h1">
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
             Transforming Ideas
             <br /> Into <span className={"text-accent"}>Digital Reality</span>
-          </h1>
+          </motion.h1>
           {/*subtitle*/}
-          <p className={"max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"}>
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className={"max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"}
+          >
             Lorem ipsum dolor sit amet. Aut explicabo architecto sit
             exercitationem delectus vel enim enim eos voluptatibus quasi sed
             cumque enim in dolore harum est iste quis?
-          </p>
+          </motion.p>
           {/*button*/}
-          <div className="flex justify-center xl:hidden">
+          <div className="flex justify-center xl:hidden relative">
             <ProjectBtn />
           </div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjectBtn />
+          </motion.div>
         </div>
       </div>
     </div>
