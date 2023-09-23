28f9cad8f44a150b6d3801b5f35e50650656af14
hesamwebdesign <hesamwebdesigner@gmail.com>
2023-09-23 21:47:55 +0330

    Add Project Button
    This will lead you to "Work" page

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,5 +1,36 @@
+// Next
+import Image from "next/image";
+import Link from "next/link";
+
+// Icons
+import { HiArrowRight } from "react-icons/hi2";
+
 const ProjectsBtn = () => {
-  return <div>Project Btn</div>;
+  return (
+    <div>
+      <Link
+        href={"/work"}
+        className={
+          "relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-no-repeat bg-cover group"
+        }
+      >
+        <Image
+          src={"/rounded-text.png"}
+          width={141}
+          height={148}
+          alt="My Projects"
+          className={
+            "animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
+          }
+        />
+        <HiArrowRight
+          className={
+            "absolute text-4xl group-hover:translate-x-1 transition-all duration-300"
+          }
+        />
+      </Link>
+    </div>
+  );
 };
 
 export default ProjectsBtn;
