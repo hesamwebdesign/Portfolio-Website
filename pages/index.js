184afe8f1c91357cbebbf98c33c884167782f0d1
hesamwebdesign <hesamwebdesigner@gmail.com>
2023-09-25 21:42:01 +0330

    Revert "Revert "Change Build statement""
    This reverts commit 26d5758e12d96d878c8aaa3baa5cf391f522880f.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 // Next
+import Head from "next/head";
 import Image from "next/image";
 
 // Framer Motion
@@ -14,57 +15,77 @@ import { FadeIn, fadeIn } from "../variants";
 
 const Home = () => {
   return (
-    <div className={"bg-primary/60 h-full"}>
-      {/*text*/}
-      <div
-        className={
-          "w-full h-full bg-gradient-to-r from-primary/10 via-black/20"
-        }
-      >
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <div className={"bg-primary/60 h-full"}>
+        {/*text*/}
         <div
           className={
-            "text-center flex flex-col justify-center h-full mx-auto container xl:pt-32 xl:text-left"
+            "w-full h-full bg-gradient-to-r from-primary/10 via-black/20"
           }
         >
-          {/*title*/}
-          <motion.h1
-            variants={fadeIn("down", 0.2)}
-            initial="hidden"
-            animate="show"
-            exit="hidden"
-            className="h1"
+          <div
+            className={
+              "text-center flex flex-col justify-center h-full mx-auto container xl:pt-32 xl:text-left"
+            }
           >
-            Transforming Ideas
-            <br /> Into <span className={"text-accent"}>Digital Reality</span>
-          </motion.h1>
-          {/*subtitle*/}
-          <motion.p
-            variants={fadeIn("down", 0.3)}
-            initial="hidden"
-            animate="show"
-            exit="hidden"
-            className={"max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"}
-          >
-            Lorem ipsum dolor sit amet. Aut explicabo architecto sit
-            exercitationem delectus vel enim enim eos voluptatibus quasi sed
-            cumque enim in dolore harum est iste quis?
-          </motion.p>
-          {/*button*/}
-          <div className="flex justify-center xl:hidden relative">
-            <ProjectBtn />
+            {/*title*/}
+            <motion.h1
+              variants={fadeIn("down", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h1"
+            >
+              Transforming Ideas
+              <br /> Into <span className={"text-accent"}>Digital Reality</span>
+            </motion.h1>
+            {/*subtitle*/}
+            <motion.p
+              variants={fadeIn("down", 0.3)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className={"max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"}
+            >
+              Lorem ipsum dolor sit amet. Aut explicabo architecto sit
+              exercitationem delectus vel enim enim eos voluptatibus quasi sed
+              cumque enim in dolore harum est iste quis?
+            </motion.p>
+            {/*button*/}
+            <div className="flex justify-center xl:hidden relative">
+              <ProjectBtn />
+            </div>
+            <motion.div
+              variants={fadeIn("down", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="hidden xl:flex"
+            >
+              <ProjectBtn />
+            </motion.div>
           </div>
-          <motion.div
-            variants={fadeIn("down", 0.4)}
-            initial="hidden"
-            animate="show"
-            exit="hidden"
-            className="hidden xl:flex"
-          >
-            <ProjectBtn />
-          </motion.div>
+        </div>
+        {/*image*/}
+        <div className={"absolute w-[1200px] h-full bottom-0 right-0"}>
+          {/*background image*/}
+          <div
+            className={
+              "w-full h-full bg-none xl:bg-explosion xl:bg-cover bg-right bg-no-repeat absolute mix-blend-color-dodge translate-z-0"
+            }
+          ></div>
+          {/*particles*/}
+          <div>Particles</div>
+          {/*avatar*/}
+          <div className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
+            <Avatar />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
