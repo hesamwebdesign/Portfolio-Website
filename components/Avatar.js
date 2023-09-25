26d5758e12d96d878c8aaa3baa5cf391f522880f
hesamwebdesign <hesamwebdesigner@gmail.com>
2023-09-25 21:32:02 +0330

    Revert "Change Build statement"
    This reverts commit b9a2cec0514f9940556d7b284dfd3546d3923fa9.

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,17 +1,5 @@
-import Image from "next/image";
-
 const Avatar = () => {
-  return (
-    <div className={"hidden xl:flex max-w-none"}>
-      <Image
-        src={"/avatar.png"}
-        width={737}
-        height={678}
-        alt="avatar"
-        className="translate-z-0 w-full h-full"
-      />
-    </div>
-  );
+  return <div>Avatar</div>;
 };
 
 export default Avatar;
